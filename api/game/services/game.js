e32f21161f25b2f2719495a7f8b0c80bd10489fa
Diego Resende <wdresende@gmail.com>
2020-09-03 01:03:00 -0300

    Finalize the populate

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,6 +7,7 @@
 
 const axios = require("axios");
 const slugify = require("slugify");
+const qs = require("querystring");
 
 function Exception(e) {
   return { e, data: e.data && e.data.errors && e.data.errors };
@@ -125,6 +126,7 @@ async function createGames(products) {
 
       if (!item) {
         console.info(`Creating: ${product.title}...`);
+
         const game = await strapi.services.game.create({
           name: product.title,
           slug: product.slug.replace(/_/g, "-"),
@@ -152,7 +154,7 @@ async function createGames(products) {
             .map((url) => setImage({ image: url, game, field: "gallery" }))
         );
 
-        await timeout(2000);
+        await timeout(4000);
 
         return game;
       }
@@ -163,11 +165,17 @@ async function createGames(products) {
 module.exports = {
   populate: async (params) => {
     try {
-      const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+      const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&${qs.stringify(
+        params
+      )}`;
+
+      console.log(gogApiUrl);
 
       const {
         data: { products },
       } = await axios.get(gogApiUrl);
+
+      // console.log(products);
 
       await createManyToManyData(products);
       await createGames(products);
